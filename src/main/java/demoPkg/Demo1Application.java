448a58c2affd9ec9afe8a06ca8c369438722f090
satbirmehta <mehta.satbir@gmail.com>
2017-07-14 11:05:43 -0400

    my first spring demo commit

---
+++ src/main/java/demoPkg/Demo1Application.java
@@ -0,0 +1,28 @@
+package demoPkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@SpringBootApplication
+public class Demo1Application
+{
+     
+	@RequestMapping("/")
+    @ResponseBody
+    String home()
+    {
+		return "Hello, World!";
+    }
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+}
+
+
+
+
